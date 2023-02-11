b6bc7410c2447bed12dc2c250c5fd197cb26e4b7
Ragnadamm <felipe92pontes@gmail.com>
2023-02-11 19:49:50 -0300

    add novos exemplos

---
+++ src/atividades/dio/Ex_1.java
@@ -0,0 +1,10 @@
+package atividades.dio;
+
+public class Ex_1 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
---
+++ src/atividades/dio/Ex_2.java
@@ -0,0 +1,8 @@
+package atividades.dio;
+
+public class Ex_2 {
+    public static void main(String[] args) {
+
+        System.out.println("\n Kawabanga!!!");
+    }
+}
